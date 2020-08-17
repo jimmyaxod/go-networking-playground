5efd9a0723eaaa6cc5be09b9a773c43d0dc2d230
Jimmy <jimmy.moore@gmail.com>
2020-08-17 18:01:21 +0100

    Going to play with noise encryption here

---
+++ noiseexample.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	noise "github.com/libp2p/go-libp2p-noise"
+	"os"
+	"fmt"
+)
+
+func main() {
+
+	/*
+	 * Do some things with the go libp2p noise library, so I can double check, and learn go in the process...
+	 */
+
+}
