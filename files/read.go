7c22f14b8c2a5d7017f4d72b756ecd989ad08d7e
Jimmy <jimmy.moore@gmail.com>
2020-08-19 13:11:25 +0100

    Fleshed out basic file reading

--- files/read.go
+++ files/read.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    "bufio"
+//    "bufio"
     "fmt"
-    "log"
+    "time"
+    "sync"
     "os"
 )
 
@@ -12,22 +13,38 @@ import (
  *
  */
 func main() {
+    var wg sync.WaitGroup
+
+    starttime := time.Now().UnixNano()
 
     a := os.Args[1:]
     for i := 0; i < len(a); i++ {
         fmt.Printf("Arg %d %s\n", i, a[i])
-	readfile(a[i])
+	wg.Add(1)
+	go readfile(&wg, a[i])
 	// We want to read it in...
     }
 
+    fmt.Println("Waiting for everything to finish")
+    wg.Wait()
+
+    endtime := time.Now().UnixNano()
+
+    fmt.Fprintf(os.Stdout, "Took %dms\n", (endtime - starttime) / 1e6)
+
+    os.Exit(0)
 }
 
 /*
  * Read file in and do something with the data...
  *
  */
-func readfile(f string) {
+func readfile(wg *sync.WaitGroup, f string) {
+    defer wg.Done()
+
     fmt.Printf("Reading %s\n", f)
+
+/*
     file, err := os.Open("/path/to/file.txt")
     if err != nil {
         log.Fatal(err)
@@ -42,4 +59,5 @@ func readfile(f string) {
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
+*/
 }
